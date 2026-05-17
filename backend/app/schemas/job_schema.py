314f5ec2d7274396f5662826a79aa29e249ec4cd
Rahmat <rahmuh09@gmail.com>
2026-05-18 00:36:27 +0430

    Pydantic Schemas for Jobs

---
+++ backend/app/schemas/job_schema.py
@@ -0,0 +1,20 @@
+from typing import Optional
+from pydantic import BaseModel
+
+class JobCreate(BaseModel):
+    title: str
+    description: str
+    requirements: str
+    location: Optional[str] = None
+
+
+class JobResponse(BaseModel):
+    id: int
+    title: str
+    description: str
+    requirements: str
+    location: Optional[str] = None
+    is_active: bool
+
+    class Cinfig:
+        from_attributes = True
