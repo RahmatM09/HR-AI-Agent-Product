3ad5910bb70e467062272ff37480c0f8bcdcc5a8
Rahmat <rahmuh09@gmail.com>
2026-05-21 03:17:00 +0430

    Building the Core Apply Route

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI
 
-from app.routes import job_routes
+from app.routes import job_routes, apply_routes
 
 app = FastAPI(title="HR AI Agent Backend")
 
 app.include_router(job_routes.router)
+app.include_router(apply_routes.router)
